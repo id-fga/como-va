926ba3b601e8255499559bc07e61db7c6f48b427
Sebastián Álvarez <salvarez@educ.gov.ar>
2016-02-23 14:52:16 -0300

    Modulo main para manejar el llamado constante a los procesos, se spawnean todos los procesos y quedan a la espera de un mensaje para iniciar

--- como-va-buscar/pings.ex
+++ como-va-buscar/pings.ex
@@ -7,35 +7,69 @@ defmodule Funcs do
     receive do
       {:connect, remitente, name, ip} -> send remitente, connect(name, ip)
     end
+    esperar
   end
 
   def connect(name, ip) do
     case Node.connect :"#{name}@#{ip}" do
-        :true -> "#{ip}"
-        :false -> :error
+        :true ->  {:ok, ip}
+        :false -> {:error, ip}
     end
   end
 end
 
-
-
-rango = 2 .. 254
-Enum.to_list(rango)
-|> Enum.map(fn (o) ->
-    pid = Funcs.start
-    send pid, {:connect, self, "mxt", "10.6.1.#{o}"}
-end)
-|> Enum.map(fn (pid) ->
-
-  receive do
-    :error -> :error
-    m -> IO.puts "#{m}"
-
-    after 50 -> :timeout
+defmodule Main do
+  def start do
+    rango = 2 .. 254
+    Enum.to_list(rango)
+    |> Enum.map(fn (o) ->
+      pid = Funcs.start
+      {pid, "mxt", "10.6.1.#{o}"}
+    end)
   end
-    
-end)
 
-l = :global.registered_names
-IO.inspect l
+  def mensajear (pid_list) do
+    Enum.to_list(pid_list)
+    |> Enum.map(fn (pid_element) -> 
+      {pid, name, ip} = pid_element
+      send pid, {:connect, self, name, ip}
+      pid
+    end)
+  end
+
+  def esperar (pid) do
+    Enum.to_list(pid)
+    |> Enum.map(fn (pid) ->
+
+      receive do
+        {:error, ip} -> :error
+        {:ok, ip} -> :ok
+
+        after 50 -> :timeout
+   
+      end
+
+    end)
+  end
+
+  def loop (pid_list) do
+    IO.puts "\nEscaneando todo"
+    mensajear pid_list
+    esperar pid_list
+
+    l = :global.registered_names
+    IO.inspect l
+    IO.puts "Termino todo\n"
+
+    loop pid_list
+  end
+
+
+end
+
+Main.start
+|> Main.loop
+  
+
+
 
