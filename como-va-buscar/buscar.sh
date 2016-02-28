a914f38252dabc7db708f092142175cf3d1d756c
Sebastian <seba.alvarez16@gmail.com>
2016-02-28 19:23:45 -0300

    Se agrega argumento para interfaz de red

--- como-va-buscar/buscar.sh
+++ como-va-buscar/buscar.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-iface=eth1
 usuario=buscar
+
+iface=$1
+if [ -z $iface ]; then
+    echo "No se especifico interfaz de red"
+    exit -1
+fi
 
 ip=$(/sbin/ifconfig $iface | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 
--- como-va-publicar/publicar.sh
+++ como-va-publicar/publicar.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-iface=eth1
+usuario=mxt
+iface=$1
+if [ -z $iface ]; then
+    echo "No se especifico interfaz de red"
+    exit -1
+fi
 
-ip=$(/sbin/ifconfig eth1 | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
-iex --name mxt@$ip --cookie de-chocolate
+ip=$(/sbin/ifconfig $iface | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+iex --name $usuario@$ip --cookie de-chocolate
