a7985f48134a2c1d61ddb719314a2d5b62a84eda
Sebastian <seba.alvarez16@gmail.com>
2016-02-28 18:49:11 -0300

    Se mejora script

--- como-va-buscar/buscar.sh
+++ como-va-buscar/buscar.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+iface=eth1
 usuario=buscar
-ip=$(sudo ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+
+ip=$(/sbin/ifconfig $iface | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 
 echo "Iniciando $usuario@$ip"
 elixir --name $usuario@$ip -r pings2.ex --cookie de-chocolate
--- como-va-publicar/publicar.sh
+++ como-va-publicar/publicar.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-ip=$(sudo ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+iface=eth1
+
+ip=$(/sbin/ifconfig eth1 | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 iex --name mxt@$ip --cookie de-chocolate
