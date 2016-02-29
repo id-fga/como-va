89d7a4f92fd541f32b05e97c50a93dd382cee8ec
Sebastian <seba.alvarez16@gmail.com>
2016-02-28 22:36:35 -0300

    Prueba con ping y despues con connect

--- como-va-buscar/buscar.sh
+++ como-va-buscar/buscar.sh
@@ -11,4 +11,5 @@ fi
 ip=$(/sbin/ifconfig $iface | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 
 echo "Iniciando $usuario@$ip"
-elixir --name $usuario@$ip -r pings2.ex --cookie de-chocolate
+#elixir --name $usuario@$ip -r pings2.ex --cookie de-chocolate
+elixir --name $usuario@$ip -r pings3.ex --cookie de-chocolate
