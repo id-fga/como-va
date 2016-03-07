41af8fab30f0e74f7623c0d38d2dac4aa7a1fb26
id-fga <investigacion.desarrollo.fga@gmail.com>
2016-03-07 01:15:10 -0300

    Se arregla spawn del socket de enviador

--- como-va.ex
+++ como-va.ex
@@ -1,14 +1,18 @@
 defmodule Sender do
     def start do
-        spawn(Sender, :repetir, [])
+        spawn(Sender, :init, [])
     end
 
-    def repetir do
-        IO.puts "Yo soy el maestro"
+    def init do
         {:ok, s} = :gen_udp.open(0, [])
+        loop(s)
+    end
+
+    def loop(s) do
+        IO.puts "Yo soy el maestro"
         :timer.sleep(1000)
         :gen_udp.send(s, {224, 1, 1, 1}, 49999, "master_node")
-        repetir
+        loop(s)
     end
 
 end
