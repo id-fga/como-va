6a469ed7435d1d504779e5ea22c8d3384879be5e
id-fga <investigacion.desarrollo.fga@gmail.com>
2016-03-14 17:40:39 -0300

    Cambios enormes, se vuelve a spawn, se usa timeout de recv, no se calcula ip menor

--- lib/como_va.ex
+++ lib/como_va.ex
@@ -2,14 +2,14 @@ defmodule MasterListener do
     use GenServer
 
     def start do
-        GenServer.start(__MODULE__, :ok, [])
+        #GenServer.start(MasterListener, :ok, [])
+        spawn(MasterListener, :init, [])
     end
 
-    def init(:ok) do
+    def init do
         udp_options = [
             :binary,
-            #active:          10,
-            active:          :true,
+            active:          false,
             add_membership:  { {224,1,1,1}, {0,0,0,0} },
             multicast_if:    {0,0,0,0},
             multicast_loop:  false,
@@ -18,50 +18,26 @@ defmodule MasterListener do
         ]
 
         {:ok, socket} = :gen_udp.open(49999, udp_options)
+        loop(socket, {})
     end
 
-    def handle_info({:udp, socket, {oct1, oct2, oct3, sender_oct}, port, "master_node"}, state) do
-        IO.puts "Proceso paquete de #{oct1}.#{oct2}.#{oct3}.#{sender_oct}"
-        #:inet.setopts(socket, [active: :once])
+    def loop(socket, master) do
 
-        string_local_ip = Enum.join(Tuple.to_list(get_ip), ".")
-        local_oct = get_oct(get_ip, 4)
+        local_ip = get_ip
 
-        IO.puts "LISTENER > " <> to_string(:erlang.system_time)
-
-        case decidir(local_oct, sender_oct) do
-            :harakiri       ->  harakiri
-            _               ->  :keepalive
+        case :gen_udp.recv(socket, 0, 5000) do
+            {:ok, {^local_ip, _, _}}                -> :ignore
+            {:ok, {sender_ip, _, "master_node"}}    ->  IO.puts "Master es #{inspect sender_ip}"
+            {:error, :timeout}                      ->  IO.puts "Estan todos callados"
+                                                        Sender.start
         end
 
-        IO.puts "----------------------------"
-
-        {:noreply, state}
-    end
-
-    def decidir(local, sender) when local > sender do
-        IO.puts "LISTENER > Local  es: " <> to_string(local)
-        IO.puts "LISTENER > Sender es: " <> to_string(sender)
-        :harakiri
-    end
-
-    def decidir(local, sender) do
-        :discard
-    end
-
-    defp get_oct(ip, pos) do
-        elem(ip, pos - 1)
+        loop(socket, {})
     end
 
     defp get_ip do
         {:ok, val} = :inet.getif() 
         elem(hd(val), 0)
-    end
-
-    defp harakiri do
-        IO.puts "LISTENER > El puede ser el maestro, debo morir"
-        main_pid = Process.whereis(:main)
-        send main_pid, :reiniciar
     end
 end
 
@@ -90,23 +66,25 @@ defmodule ComoVa do
     end
 
     def iniciar do
-        sender_pid = Sender.start
-        {:ok, listener_pid} = MasterListener.start
+        #sender_pid = Sender.start
+        listener_pid = MasterListener.start
 
         #TODO: Hacer con una lista de tuplas, iterando y registrando
         Process.register(self, :main)
-        Process.register(sender_pid, :sender)
+        #Process.register(sender_pid, :sender)
         Process.register(listener_pid, :listener)
 
         #TODO: Rehacer, revisar unregister, agregar sleep random
         receive do
-            :reiniciar  ->  Process.exit(Process.whereis(:sender), :kill)
-                            Process.exit(Process.whereis(:listener), :kill)
-                            Process.unregister(:main)
-                            IO.puts "MAIN > Todo esta muerto"
-                            IO.puts "MAIN > Espero y vuelvo a arrancar"
-                            :timer.sleep(5000)
-                            iniciar
+            #:reiniciar  -> #Process.exit(Process.whereis(:sender), :kill)
+                            #Process.exit(Process.whereis(:listener), :kill)
+                            #Process.unregister(:main)
+                            #IO.puts "MAIN > Todo esta muerto"
+                            #IO.puts "MAIN > Espero y vuelvo a arrancar"
+                            #:timer.sleep(5000)
+                            #iniciar
+
+            _ -> :nada
         end
     end
 end
